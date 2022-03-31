1df905692ff394851fcfe4b016d7077e8cb5f2e3
Claude Barde <lotolosenc.oc@gmail.com>
2022-03-31 16:34:39 +0100

    Added illustrations and placeholders for landing page

--- frontend/src/lib/Home.svelte
+++ frontend/src/lib/Home.svelte
@@ -2,6 +2,22 @@
   import store from "../store";
 </script>
 
+<style lang="scss">
+  @import "../styles/settings.scss";
+  .landing-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 20px;
+    grid-gap: 60px;
+    align-items: center;
+
+    img {
+      width: 40vw;
+      border-radius: $std-border-radius;
+    }
+  }
+</style>
+
 <main class="central-container">
   {#if $store.userAddress.isSome()}
     <!-- User is connected -->
@@ -54,8 +70,52 @@
     </div>
   {:else}
     <!-- User is not connected -->
-    <div>Create a new blog</div>
+    <div class="landing-grid">
+      <div>
+        <img
+          src="vecteezy_illustration-vector-graphic-cartoon-character-of-home-study_6584582.jpg"
+          alt="girl-laptop"
+        />
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div>
+        <img
+          src="vecteezy_illustration-vector-graphic-cartoon-character-of-home-study_6584649.jpg"
+          alt="girl-laptop"
+        />
+      </div>
+    </div>
+    <button
+      class="primary"
+      class:light={$store.theme === "light"}
+      class:dark={$store.theme === "dark"}
+    >
+      <span class="material-icons-outlined"> add_circle_outline </span>
+      &nbsp; Create a new blog
+    </button>
     <div>Browse blogs</div>
   {/if}
   <footer>&nbsp;</footer>
+
+  <!--
+    <a href="https://www.vecteezy.com/free-vector/human">Human Vectors by Vecteezy</a>
+    <a href="https://www.vecteezy.com/free-vector/human">Human Vectors by Vecteezy</a>
+  -->
 </main>
